4c1bb9afde50a6a676062ad63186c41133280959
adonbiju <adonbiju@gmail.com>
2023-02-01 19:01:31 +0530

    Navbar Web design view has been completed

--- resturant_ui_design_vite_react/src/components/Navbar/Navbar.jsx
+++ resturant_ui_design_vite_react/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './Navbar.css'
-
+import {gericht} from '../../constants/images.js'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+   <nav className='app__navbar'>
+    <div className="app__navbar-logo">
+      <img src={gericht} alt='app__navbar-logo'/>
+    </div>
+    <ul className='app__navbar-links'>
+        <li className="p__opensans"><a href="#home">Home</a></li>
+        <li className="p__opensans"><a href="#about">About</a></li>
+        <li className="p__opensans"><a href="#menu">Menu</a></li>
+        <li className="p__opensans"><a href="#awards">Awards</a></li>
+        <li className="p__opensans"><a href="#contact">Contact</a></li>
+    </ul>
+    <div className='app__navbar-login'>
+      <a href="#login" className="p__opensans">Log In / Registration</a>
+      <div/>
+      <a href="/" className="p__opensans">Book Table</a>
+    </div>
+    
+   </nav>
   )
 }
 
