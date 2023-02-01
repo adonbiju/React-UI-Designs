4628612450388926962b5457b11b60061faac2a3
adonbiju <adonbiju@gmail.com>
2023-02-01 19:57:40 +0530

    In Navbar, Small Screen Design design added. And completed Navbar Component.

--- resturant_ui_design_vite_react/src/components/Navbar/Navbar.jsx
+++ resturant_ui_design_vite_react/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
 import {gericht} from '../../constants/images.js'
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = React.useState(false);
   return (
    <nav className='app__navbar'>
     <div className="app__navbar-logo">
@@ -19,6 +23,26 @@ const Navbar = () => {
       <div/>
       <a href="/" className="p__opensans">Book Table</a>
     </div>
+
+    <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)} />
+        {toggleMenu && (
+          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+            <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
+            <ul className="app__navbar-smallscreen_links">
+              <li><a href="#home" onClick={() => setToggleMenu(false)}>Home</a></li>
+              <li><a href="#about" onClick={() => setToggleMenu(false)}>About</a></li>
+              <li><a href="#menu" onClick={() => setToggleMenu(false)}>Menu</a></li>
+              <li><a href="#awards" onClick={() => setToggleMenu(false)}>Awards</a></li>
+              <li><a href="#contact" onClick={() => setToggleMenu(false)}>Contact</a></li>
+            </ul>
+          </div>
+        )}
+
+       </div>
+       
+
+  
     
    </nav>
   )
